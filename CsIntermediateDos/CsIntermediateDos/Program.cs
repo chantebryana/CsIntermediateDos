307b990150ef25ae610cec1b1830430221d2487b
chantebryana <chantebryana@gmail.com>
2019-09-03 07:57:07 -0500

    some stopwatch work

--- CsIntermediateDos/CsIntermediateDos/Program.cs
+++ CsIntermediateDos/CsIntermediateDos/Program.cs
@@ -2,11 +2,49 @@
 
 namespace CsIntermediateDos
 {
+    class StopwatchHP
+    {
+        private DateTime _starting;
+        private DateTime _stopping;
+
+        public DateTime Starting { get; set; }
+        public DateTime Stopping { get; set; }
+
+        public void LogTime(string word)
+        {
+            if (word == "start")
+            {
+                _starting = DateTime.Now;
+                Console.WriteLine("Start: {0}", _starting);
+            }
+            else if (word == "stop")
+            {
+                _stopping = DateTime.Now;
+                Console.WriteLine("Stop: {0}", _stopping);
+            }
+            else
+            {
+                Console.WriteLine("Please type 'start' or 'stop'.");
+            }
+        }
+
+        public TimeSpan Calculate(DateTime starting, DateTime stopping)
+        {
+            return stopping.Subtract(starting);
+        }
+
+    }
+
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            var stopwatch = new StopwatchHP();
+            string userInput;
+            Console.WriteLine("Type 'start' or 'stop': ");
+            userInput = Console.ReadLine();
+            userInput = userInput.ToLower();
+            stopwatch.LogTime(userInput);
         }
     }
 }
