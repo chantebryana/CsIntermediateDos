e37480db2f81c121f0bcd228e24bb491e0ddd8d9
chantebryana <chantebryana@gmail.com>
2019-09-16 12:53:46 -0500

    error handling and better encapsulation. fulfill requirements of exercise. (w jims help)

--- CsIntermediateDos/CsIntermediateDos/Program.cs
+++ CsIntermediateDos/CsIntermediateDos/Program.cs
@@ -5,32 +5,36 @@ namespace CsIntermediateDos
     class StopwatchHP
     {
         private DateTime _start;
-        private DateTime _stop;
 
-        public DateTime Start { get; set; }
-        public DateTime Stop { get; set; }
+        //public StopwatchHP ()
+        //{
+        //}
 
-        public DateTime PressStart ()
+        //public DateTime Start { get; set; }
+
+        public void PressStart ()
         {
+            if (_start != DateTime.MinValue)
+            {
+                throw new InvalidOperationException();
+            }
             _start = DateTime.Now;
-            return _start;
         }
-        public DateTime PressStop ()
+        public TimeSpan PressStop ()
         {
-            _stop = DateTime.Now;
-            return _stop;
-        }
-
-        public DateTime ResetStart ()
-        {
-            _start = new DateTime(0001, 01, 01);
-            return _start;
+            if (_start == DateTime.MinValue)
+            {
+                throw new InvalidOperationException();
+            }
+            TimeSpan span = DateTime.Now.Subtract(_start);
+            ResetStart();
+            return span;
         }
 
-        public DateTime ResetStop ()
+        private DateTime ResetStart ()
         {
-            _stop = new DateTime(0001, 01, 01);
-            return _stop;
+            _start = DateTime.MinValue;
+            return _start;
         }
     }
 
@@ -53,28 +57,23 @@ namespace CsIntermediateDos
                 }
                 else if (userInput == "start")
                 {
-                    if (stopwatch.Start == new DateTime(0001,01,01))
+                    try
                     {
-                        stopwatch.Start = stopwatch.PressStart();
-                        Console.WriteLine("Stopwatch started at {0}\n", stopwatch.Start);
+                        stopwatch.PressStart();
                     }
-                    else
+                    catch (InvalidOperationException e)
                     {
                         Console.WriteLine("You've already started the stopwatch; 'start' is an invalid selection.\n");
                     }
                 }
                 else if (userInput == "stop")
                 {
-                    if (stopwatch.Start != new DateTime(0001,01,01))
+                    try
                     {
-                        stopwatch.Stop = stopwatch.PressStop();
-                        TimeSpan span = stopwatch.Stop.Subtract(stopwatch.Start);
-                        Console.WriteLine("Stopwatch stopped at {0}", stopwatch.Stop);
-                        Console.WriteLine("Total time elapsed:{0}\n", span);
-                        stopwatch.Start = stopwatch.ResetStart();
-                        stopwatch.Stop = stopwatch.ResetStop();
+                        var a = stopwatch.PressStop();
+                        Console.WriteLine("Total time elapsed: {0}", a);
                     }
-                    else
+                    catch (InvalidOperationException e)
                     {
                         Console.WriteLine("You haven't started the stopwatch yet; 'stop' is an invalid selection.\n");
                     }
